1e5c3e66e7e0277b8ee948a521ef7c8770530e43
partha360 <partha@finiteloop.io>
2018-06-11 12:00:29 +0530

    Added prettier & eslint

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const app = express();
-const router = require('./router');
-const mongoose = require('mongoose');
-const config = require('./config');
-const cors = require('cors');
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express()
+const router = require('./router')
+const mongoose = require('mongoose')
+const config = require('./config')
+const cors = require('cors')
 // db setup
 
-mongoose.connect(config.mongoConnectionString);
+mongoose.connect(config.mongoConnectionString)
 
-//App Setup
-app.use(morgan('combined'));
-app.use(cors());
-app.use(bodyParser.json({ type: '*/*' }));
-router(app);
+// App Setup
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json({ type: '*/*' }))
+router(app)
 
-//Server Setup
+// Server Setup
 
-const port = process.env.PORT || 3090;
-const server = http.createServer(app);
-server.listen(port);
-console.info('Server running on: ' + port);
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
+server.listen(port)
+console.info('Server running on: ' + port)
--- router.js
+++ router.js
@@ -1,14 +1,13 @@
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport')
-const passport = require('passport');
+const Authentication = require('./controllers/authentication')
+const passportService = require('./services/passport') // eslint-disable-line no-unused-vars
+const passport = require('passport')
+const requireAuth = passport.authenticate('jwt', { session: false })
+const requireSignin = passport.authenticate('local', { session: false })
 
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignin = passport.authenticate('local', { session: false });
-
-module.exports = function (app) {
-    app.get('/', requireAuth, function (req, res) {
-        res.send({ message: 'Here is your secred code PARTHA' });
-    });
-    app.post('/signin', requireSignin, Authentication.signin);
-    app.post('/signup', Authentication.signup);
+module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ message: 'Here is your secret code PARTHA' })
+  })
+  app.post('/signin', requireSignin, Authentication.signin)
+  app.post('/signup', Authentication.signup)
 }

8b5dc8b61e6f0c492e71159c8b09c5e96bce011b
partha360 <partha@finiteloop.io>
2018-06-26 20:37:30 +0530

    Commit config files for CI

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// App secrets and configs
+
+module.exports = {
+  secret: 'louaflijibahyuhenfljouwkabkjorhrf',
+  mongoConnectionString:
+    'mongodb://partha360:calvin51@parthacluster-shard-00-00-six5v.mongodb.net:27017,parthacluster-shard-00-01-six5v.mongodb.net:27017,parthacluster-shard-00-02-six5v.mongodb.net:27017/auth?ssl=true&replicaSet=ParthaCluster-shard-0&authSource=admin'
+}
